47aadcfe5775fcd7204fc6faa7ad5c4c50285753
Elise Wei <elise.wei@gmail.com>
2024-05-05 13:09:40 +1200

    Deletion button animation + state

--- src/Tag.js
+++ src/Tag.js
@@ -1,15 +1,28 @@
 import * as React from 'react';
 
 export function Tag({ user, tag, removeTag }) {
+  const [isDeleting, setIsDeleting] = React.useState(false);
   return (
     <li className={`tag color-${tag.color}`}>
       {tag.title}
       <button
+        className={isDeleting ? 'deleting' : null}
         onClick={() => {
-          removeTag.mutate({ userId: user.uuid, tagId: tag.uuid });
+          setIsDeleting(true);
+          removeTag.mutate({
+            userId: user.uuid,
+            tagId: tag.uuid,
+          });
         }}
         title="Delete"
-      />
+      >
+        <div className="lds-ring">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </button>
     </li>
   );
 }
